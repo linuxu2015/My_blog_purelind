912197aaa0090fa57959dfd94e996b5029edb6f5
徐立宝 <xulibao@aioute.com>
2018-04-05 22:20:52 +0800

    添加wsgi文件

---
+++ wsgi.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import os
+
+from app import create_app
+import datetime
+
+# Create an application instance that web servers can use. We store it as
+# "application" (the wsgi default) and also the much shorter and convenient
+# "app".
+application = app = create_app('default')
+
+@app.context_processor
+def template_extras():
+    """
+    上下文处理装饰器，返回的字典的键可以在上下文中使用
+    :return:
+    """
+    return {'enumerate': enumerate, 'len': len, 'datetime': datetime}
